4edf6e4d126649c902aa13d7bc6f595b6bb9a5db
Marc Feeley <feeley@iro.umontreal.ca>
2014-05-24 09:15:27 -0400

    add information on runways for airports around Seattle

--- proto/airports.py
+++ proto/airports.py
@@ -2,20 +2,171 @@
 
 from geodetic import Location, LatLon
 
-runways = {}
 
-# KSEA Seattle Tacoma International
+# Creates a Runway
 
-runways['KSEA_16L'] = Location( 46.02, LatLon(47.46379733, -122.30775222))
-runways['KSEA_34R'] = Location(121.92, LatLon(47.43117533, -122.30804122))
-runways['KSEA_16C'] = Location( 67.97, LatLon(47.46381133, -122.31098822))
-runways['KSEA_34C'] = Location(120.09, LatLon(47.43797233, -122.31121322))
-runways['KSEA_16R'] = Location( 49.99, LatLon(47.46383763, -122.31786263))
-runways['KSEA_34L'] = Location(120.09, LatLon(47.44053695, -122.31806463))
+class Runway:
 
-# KTCM McChord Air Force Base
+    def __init__(self, name, reverse, location):
 
-runways['KTCM_16' ] = Location(304.80, LatLon(47.15150600, -122.47655000))
-runways['KTCM_34' ] = Location(304.80, LatLon(47.12381400, -122.47638100))
-runways['KTCM_16R'] = Location(  0.00, LatLon(47.14780800, -122.47741100))
-runways['KTCM_34L'] = Location(  0.00, LatLon(47.13874200, -122.47734700))
+        # name: name of runway
+        # reverse: name of reverse runway
+        # location: Location giving altitute, latitude and longitude
+
+        self.name = name
+        self.reverse = reverse
+        self.location = location
+
+
+# Creates an Airport
+
+class Airport:
+
+    def __init__(self, name, runways):
+
+        # name: name of airport
+        # runways: dictionnary of runways
+
+        self.name = name
+        self.runways = runways
+
+    def runway_heading(self, name):
+
+        # name: name of runway
+
+        return self.runways[name].location.latlon.bearing(self.runways[self.runways[name].reverse].location.latlon)
+
+    def runway_length(self, name):
+
+        # name: name of runway
+
+        return self.runways[name].location.latlon.distance(self.runways[self.runways[name].reverse].location.latlon)
+
+
+airports = {
+
+  # KSEA Seattle Tacoma International
+  'KSEA': Airport('KSEA', {
+                  '16L':
+                     Runway('16L',
+                            '34R',
+                            Location(433, LatLon(47.46379733, -122.30775222))),
+                  '34R':
+                     Runway('34R',
+                            '16L',
+                            Location(347, LatLon(47.43117533, -122.30804122))),
+                  '16C':
+                     Runway('16C',
+                            '34C',
+                            Location(430, LatLon(47.46381133, -122.31098822))),
+                  '34C':
+                     Runway('34C',
+                            '16C',
+                            Location(363, LatLon(47.43797233, -122.31121322))),
+                  '16R':
+                     Runway('16R',
+                            '34L',
+                            Location(415, LatLon(47.46383763, -122.31786263))),
+                  '34L':
+                     Runway('34L',
+                            '16R',
+                            Location(356, LatLon(47.44053695, -122.31806463)))
+                  }),
+
+  # KBFI Boeing Field King Co Intl
+  'KBFI': Airport('KBFI', {
+                  '13L':
+                     Runway('13L',
+                            '31R',
+                            Location( 18, LatLon(47.53799292, -122.30746100))),
+                  '31R':
+                     Runway('31R',
+                            '13L',
+                            Location( 17, LatLon(47.52916792, -122.30000000))),
+                  '13R':
+                     Runway('13R',
+                            '31L',
+                            Location( 17, LatLon(47.54051792, -122.31135600))),
+                  '31L':
+                     Runway('31L',
+                            '13R',
+                            Location( 21, LatLon(47.51672592, -122.29124200)))
+                  }),
+
+  # KRNT Renton Muni
+  'KRNT': Airport('KRNT', {
+                  '16':
+                     Runway('16',
+                            '34',
+                            Location( 24, LatLon(47.50047200, -122.21685300))),
+                  '34':
+                     Runway('34',
+                            '16',
+                            Location( 32, LatLon(47.48579400, -122.21463300)))
+                  }),
+
+  # KTCM McChord Air Force Base
+  'KTCM': Airport('KTCM', {
+                  '16':
+                     Runway('16',
+                            '34',
+                            Location(286, LatLon(47.15150600, -122.47655000))),
+                  '34':
+                     Runway('34',
+                            '16',
+                            Location(322, LatLon(47.12381400, -122.47638100)))
+                  }),
+
+  # S36 Crest Airpark
+  'S36':  Airport('S36', {
+                  '15':
+                     Runway('15',
+                            '33',
+                            Location(472, LatLon(47.34154700, -122.10457500))),
+                  '33':
+                     Runway('33',
+                            '15',
+                            Location(472, LatLon(47.33264400, -122.10249700)))
+                  }),
+
+  # S50 Auburn Muni
+  'S50':  Airport('S50', {
+                  '16':
+                     Runway('16',
+                            '34',
+                            Location( 63, LatLon(47.33234200, -122.22670300))),
+                  '34':
+                     Runway('34',
+                            '16',
+                            Location( 63, LatLon(47.32302800, -122.22660600)))
+                  })
+
+}
+
+
+def runway_heading(airport_and_runway):
+    airport = airport_and_runway[0]
+    runway = airport_and_runway[1]
+    return airports[airport].runway_heading(runway)
+
+def runway_length(airport_and_runway):
+    airport = airport_and_runway[0]
+    runway = airport_and_runway[1]
+    return airports[airport].runway_length(runway)
+
+def runway_location(airport_and_runway):
+    airport = airport_and_runway[0]
+    runway = airport_and_runway[1]
+    return airports[airport].runways[runway].location
+
+def closest_runway(latlon):
+    min_dist = float("+inf")
+    min_rw = None
+    for airport in airports:
+        for runway in airports[airport].runways:
+            rw = (airport, runway)
+            d = runway_location(rw).latlon.distance(latlon)
+            if (d < min_dist):
+                min_dist = d
+                min_rw = rw
+    return min_rw
